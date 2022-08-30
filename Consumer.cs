f03c419c9ab872b8d5b28c251c71ea34445ba260
VIA\2160010917 <victor.ratuchenei@via.com.br>
2022-08-29 23:26:02 -0300

    Include git build

--- Consumer.cs
+++ Consumer.cs
@@ -1,31 +1,26 @@
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Kafka;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace FunctionKafkaConsumer
 {
     public class Consumer
-    { 
-        // KafkaTrigger sample 
-        // Consume the message from "topic" on the LocalBroker.
-        // Add `BrokerList` and `KafkaPassword` to the local.settings.json
-        // For EventHubs
-        // "BrokerList": "{EVENT_HUBS_NAMESPACE}.servicebus.windows.net:9093"
-        // "KafkaPassword":"{EVENT_HUBS_CONNECTION_STRING}
+    {
+        const string BROKER = "localhost:9092";   //"10.128.145.106:9092";
+        const string TOPIC = "via-mais-figital";
+        const string CONSUMER_GROUP = "via-martech-viamais-dev";
+
         [FunctionName("Consumer")]
-        public void Run(
-            [KafkaTrigger("localhost:9092",
-                          "test-topic",
-                          //Username = "teste",   //"$ConnectionString",
-                          //Password = "",        //"%KafkaPassword%",
-                          Protocol = BrokerProtocol.Plaintext,
-                          AuthenticationMode = BrokerAuthenticationMode.NotSet,
-                          ConsumerGroup = "test")] KafkaEventData<string>[] events, ILogger log)
+        public void Run([KafkaTrigger(BROKER, TOPIC, ConsumerGroup = CONSUMER_GROUP)] KafkaEventData<string>[] events, ILogger log)
         {
+            var teste = Environment.GetEnvironmentVariable("IsEncrypted");
+
             foreach (KafkaEventData<string> eventData in events)
             {
                 log.LogInformation($"C# Kafka trigger function processed a message: {eventData.Value}");
             }
+            
         }
     }
 }
---
+++ Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionKafkaConsumer;
+
+public class Startup : FunctionsStartup
+{
+    //IsEncrypted
+
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        var teste = Environment.GetEnvironmentVariable("IsEncrypted");
+
+
+        throw new NotImplementedException();
+    }
+}
